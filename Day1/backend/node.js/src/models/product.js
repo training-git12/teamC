ca4792fc3c2f0e623c3c7f77201939d6a84437c1
Miki Yoshida <Miki.Yoshida@ibm.com>
2024-12-11 15:15:25 +0900

    productmodel修正

--- Day1/backend/node.js/src/models/product.js
+++ Day1/backend/node.js/src/models/product.js
@@ -1,6 +1,7 @@
 const mongoose = require('../config/database'); 
 
 const productSchema = new mongoose.Schema({
+    id: { type: String, required: true},
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
@@ -8,7 +9,17 @@ const productSchema = new mongoose.Schema({
     type: { type: String, required: true },
     brand: { type: String, required: true },
     stock: { type: Number, required: true },
-    images: { type: [String], required: true }
+    images: { type: [String], required: true },
+    rating: { type: Number, required: true},
+    specifications: { type: Object, requre: true},
+    discount: { type: Number, required: true},
+    created_at: { type: Date, required: true},
+    updated_at: { type: DataTransfer, required: true},
+    tags: { type: [String], required: true},
+    seller_id: { type: String, required: true},
+    likes: { type: String, required: true},
+    __v: { type: Number, required: true},
+    reviews: { type: [String], required: true}
 });
 
 const Product = mongoose.model('Product', productSchema);
