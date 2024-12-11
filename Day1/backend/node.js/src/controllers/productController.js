3c944843e506621b97de8ca649642ed4b31df154
Rei-Taniguchi <Rei.Taniguchi@ibm.com>
2024-12-11 15:16:23 +0900

    API

--- Day1/backend/node.js/src/controllers/productController.js
+++ Day1/backend/node.js/src/controllers/productController.js
@@ -1,19 +1,24 @@
 const Product = require('./models/product'); 
 
-const getAllProducts = async () => {
+exports.getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find(); 
-        return products;
-    } catch (err) {
-        console.error('Error fetching products:', err);
-        throw err; 
+        const products = await Product.find({});
+        res.json(products);
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).send(error);
     }
 };
 
-getAllProducts()
-    .then((products) => {
-        console.log('Products:', products);
-    })
-    .catch((err) => {
-        console.error('Failed to get products:', err);
-    });
+exports.getProductById = async (req, res) => {
+    const { productId } = req.params;
+    
+    try {
+        const product = await Product.findById(productId);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+        res.json(product);
+    } catch (error) {
+        console.error("Error fetching product:", error);
+        res.status(500).send(error);
+    }
+};
